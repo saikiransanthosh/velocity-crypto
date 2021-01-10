155f070fd62836e997d9ffcdaeb51bb8e106bd78
saikiran.velankayala <saikiran.velankayala@servicenow.com>
2021-01-10 12:47:55 +0530

    Updates

---
+++ precache-manifest.63d495307d234271c321b3bd11e1cf35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55116b5065916038a8fa",
+    "url": "/velocity-crypto/static/css/main.a84bacfd.chunk.css"
+  },
+  {
+    "revision": "55116b5065916038a8fa",
+    "url": "/velocity-crypto/static/js/main.55116b50.chunk.js"
+  },
+  {
+    "revision": "95cb6d42465cd489c6ff",
+    "url": "/velocity-crypto/static/css/1.7fc9cf61.chunk.css"
+  },
+  {
+    "revision": "95cb6d42465cd489c6ff",
+    "url": "/velocity-crypto/static/js/1.95cb6d42.chunk.js"
+  },
+  {
+    "revision": "a2e979da9106dd56a919",
+    "url": "/velocity-crypto/static/js/runtime~main.a2e979da.js"
+  },
+  {
+    "revision": "602b4803207df91f3dbab03cc28ab1a3",
+    "url": "/velocity-crypto/index.html"
+  }
+];
